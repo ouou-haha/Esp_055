a8a030ff8aa900a9d31802b0c143a09850d6c2d0
ouou-haha <ou471072445@163.com>
2026-01-08 00:45:29 +0800

    add wifi connect tmp

---
+++ main/wifi_connect.c
@@ -0,0 +1,119 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "wifi_connect.h"
+
+
+#define WIFI_SSID "huawei"
+#define WIFI_PASS "@ouhaha6666"
+#define WIFI_MAX_RETRY 10
+
+static const char *TAG = "wifi_connect";
+
+static EventGroupHandle_t s_wifi_event_group;
+static int s_retry_num = 0;
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+
+static esp_netif_t *s_sta_netif = NULL;
+#define EXAMPLE_NETIF_DESC_STA "example_netif_sta"
+
+static void wifi_event_handler(void *arg,
+                               esp_event_base_t event_base,
+                               int32_t event_id,
+                               void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < WIFI_MAX_RETRY) {
+            s_retry_num++;
+            ESP_LOGW(TAG, "wifi disconnected, retry %d/%d", s_retry_num, WIFI_MAX_RETRY);
+            esp_wifi_connect();
+        } else {
+            ESP_LOGE(TAG, "wifi connect failed");
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "got ip: " IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+static esp_err_t init_wifi(void) {
+    s_wifi_event_group = xEventGroupCreate();
+    if (!s_wifi_event_group) return ESP_ERR_NO_MEM;
+
+    /* init wifi driver */
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    /* capture actual connection details (IP, gw, mask) */
+    esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_STA();
+    esp_netif_config.if_desc = EXAMPLE_NETIF_DESC_STA;
+    esp_netif_config.route_prio = 128;
+    s_sta_netif = esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
+    esp_wifi_set_default_wifi_sta_handlers();
+
+    /* set default wifi sta handlers */
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL));
+    
+    /* where to store wifi config */
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+    return ESP_OK;
+}
+
+esp_err_t wifi_connect(void)
+{
+    ESP_ERROR_CHECK(init_wifi());
+     
+    wifi_config_t wifi_config = {
+        .sta = {
+            .scan_method = WIFI_FAST_SCAN, /* WIFI_ALL_CHANNEL_SCAN */
+            .sort_method = WIFI_CONNECT_AP_BY_SIGNAL, /* WIFI_CONNECT_AP_BY_SECURITY */
+            .threshold.rssi = -127,
+            // .threshold.authmode = EXAMPLE_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+        },
+    };
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    wifi_config.sta.pmf_cfg.capable = true;
+    wifi_config.sta.pmf_cfg.required = false;
+
+    strncpy((char *)wifi_config.sta.ssid, WIFI_SSID, sizeof(wifi_config.sta.ssid));
+    strncpy((char *)wifi_config.sta.password, WIFI_PASS, sizeof(wifi_config.sta.password));
+
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_start, connecting to ssid=%s", WIFI_SSID);
+
+    /* block and wait for the result */
+    EventBits_t bits = xEventGroupWaitBits(
+        s_wifi_event_group,
+        WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+        pdFALSE,
+        pdFALSE,
+        portMAX_DELAY
+    );
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "wifi connected");
+        return ESP_OK;
+    } else {
+        ESP_LOGE(TAG, "wifi failed");
+        return ESP_FAIL;
+    }
+}
